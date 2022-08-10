09b835dd66cb0d6fe1aaa3d6d6ed1002d0baf244
EdwinAyala22 <esayala86@misena.edu.co>
2022-08-10 11:30:41 -0500

    Se crearon los Hooks

--- src/Pages/Characters/index.js
+++ src/Pages/Characters/index.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect} from 'react';
+import { usePerson } from '../../Hooks/usePerson';
 
 export const Characters = () => {
+// const [isLoading, setIsLoading] = useState(true);
+// const [personajes, setPersonajes] = useState([]);
+
+  const {getAllPerson,dataPeople} = usePerson();
+
+  useEffect(()=>{
+    getAllPerson();
+    // let data = [dataPeople];
+    // data[0].map((result) => (console.log(result.count)) )
+    let data = Object.keys(dataPeople);
+    for (let i = 0; i < data.length; i++) {
+      let clave = data[i];
+      console.log(dataPeople[clave])
+      
+    }
+  }, []);
   return (
-    <div>Los personajes xd</div>
+    <div>
+      
+    </div>
   )
 }
